d04c0668f2aab10f1360efd901cb46be6a121b8c
onidemon37 <41202410+onidemon37@users.noreply.github.com>
2022-06-27 22:39:35 +0100

    set to hold server/ip combinations, and add from JSON file

--- servercheck/cli.py
+++ servercheck/cli.py
@@ -9,6 +9,28 @@ def cli(filename, server):
     if not filename and not server:
         raise click.UsageError("Must provide a JSON file or servers")
 
+    # create a set to prevent duplicate server /port combinations
+    servers = set()
+
+    # If --filename or -f option is used then attempt to read
+    # the file and add all values to the `servers` set.
+    if filename:
+        try:
+            with open(filename) as f:
+                json_servers = json.load(f)
+                for s in json_servers:
+                    servers.add(s)
+        except:
+            print("Error: Unable to open or read JSON file")
+            sys.exit(1)
+
+    # If --server or -s options are used then add those values
+    if server:
+        for s in server:
+            server.add(s)
+
+    print(servers)
+
 
 if __name__ == "__main__":
     cli()
